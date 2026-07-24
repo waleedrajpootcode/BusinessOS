8ddeab377b0569e3a1595e084ca4aa408a89554b
waleedrajpootcode <waleedrajpoot0096w@gmail.com>
2026-07-24 19:41:20 +0500

    Show/Hide Password

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,43 @@
+import { useState } from 'react'
+import './Login.css'
+
 function Login() {
+  const [showPassword, setShowPassword] = useState(false)
   return (
-  <div className="login">
-    <h1>Login</h1>
-    <p>Welcome back to BusinessOS</p>
-  </div>
-)
+    <div className="login-page">
+
+      <div className="login-card">
+
+        <h1>BusinessOS</h1>
+
+        <p>AI Powered Business Management SaaS</p>
+
+        <form>
+
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Enter your email"
+          />
+
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+          />
+
+          <a href="#">Forgot Password?</a>
+
+          <button type="submit">
+            Login
+          </button>
+
+        </form>
+
+      </div>
+
+    </div>
+  )
 }
 
 export default Login
